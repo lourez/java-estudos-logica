c990e169f133644cbe211c5c589d826ffa3f678a
Arthur Lourenço <wal271223@gmail.com>
2025-07-07 20:13:27 -0300

    feat: Novo exercício completo

--- src/arrays/CalculoDeDoisNumeros.java
+++ src/arrays/CalculoDeDoisNumeros.java
@@ -6,30 +6,30 @@ public class CalculoDeDoisNumeros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int array1[] = new int[3];
-        int array2[] = new int[3];
-        int resp[] = new int[3];
+        int numero1[] = new int[3];
+        int numero2[] = new int[3];
+        int resultadoMultiplicacao[] = new int[3];
 
         System.out.println("----------------------");
         System.out.println("VETOR 1");
         System.out.println("----------------------");
-        for (int i = 0; i < array1.length; i++) {
+        for (int i = 0; i < numero1.length; i++) {
             System.out.print("Digite o " + (i + 1) + "º número: ");
-            array1[i] = sc.nextInt();
+            numero1[i] = sc.nextInt();
         }
         System.out.println("----------------------");
         System.out.println("VETOR 2");
         System.out.println("----------------------");
-        for (int i = 0; i < array2.length; i++) {
+        for (int i = 0; i < numero2.length; i++) {
             System.out.print("Digite o " + (i + 1) + "º número: ");
-            array2[i] = sc.nextInt();
+            numero2[i] = sc.nextInt();
         }
         System.out.println("----------------------");
         System.out.println("MULTIPLICAÇÃO");
         System.out.println("----------------------");
-        for (int i = 0; i < resp.length; i++) {
-            resp[i] = array1[i] * array2[i];
-            System.out.println("O resultado da multiplicação de " + array1[i] + " X " + array2[i] + " é: " + resp[i]);
+        for (int i = 0; i < resultadoMultiplicacao.length; i++) {
+            resultadoMultiplicacao[i] = numero1[i] * numero2[i];
+            System.out.println("O resultado da multiplicação de " + numero1[i] + " X " + numero2[i] + " é: " + resultadoMultiplicacao[i]);
         }
     }
 }
