68e5b21dacc68b6ec0a050c8ab9e3729f88801ae
Arthur Lourenço <wal271223@gmail.com>
2025-07-07 22:09:12 -0300

    feat: Exercicio sobre arrays Multidimensionais

---
+++ src/arraysMultidimensionais/somaMatriz.java
@@ -0,0 +1,35 @@
+package arraysMultidimensionais;
+
+import java.util.Scanner;
+
+public class somaMatriz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matriz = new int[3][3];
+        int soma = 0;
+
+        System.out.println("----------");
+        System.out.println("Preencha a matriz 3x3:");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("----------");
+        System.out.println("A matriz preenchida é:");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+                soma += matriz[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
+        System.out.println("A soma dos elementos da matriz é: " + soma);
+    }
+}
+
